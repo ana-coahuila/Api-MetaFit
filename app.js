68840466b020df9d628b1fe26f11417f8b1c5a3d
AnaCoahuila <al222210983@gmail.com>
2025-10-15 10:55:11 -0600

    actualizacion de variables

--- app.js
+++ app.js
@@ -7,22 +7,14 @@ const exerciseRoutes = require('./routes/exerciseRoutes');
 const planRoutes = require('./routes/planRoutes');
 const progressRoutes = require('./routes/progressRoutes');
 const errorHandler = require('./middleware/errorHandler');
+require('dotenv').config();
 
 const app = express();
-const PORT = 5000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 app.use(errorHandler);
-
-// Conexión a MongoDB
-mongoose.connect('mongodb://localhost:27017/fitness_db', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('Conectado a MongoDB'))
-.catch(err => console.error('Error de conexión a MongoDB:', err));
 
 // Rutas
 app.use('/api/auth', authRoutes);
@@ -32,6 +24,16 @@ app.use('/api/plans', planRoutes);
 app.use('/api/progress', progressRoutes);
 
 // Rutas para la ia 
+
+// Conexión a MongoDB
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/fitness_db';
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log('Conectado a MongoDB'))
+.catch(err => console.error('Error de conexión a MongoDB:', err));
+
+// Puerto
+const PORT = process.env.PORT || 5000;
+const HOST = process.env.HOST || '0.0.0.0';
 
 // Iniciar servidor
 app.listen(PORT, () => {
